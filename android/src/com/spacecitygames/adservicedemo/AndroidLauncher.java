042ea245387af0c133cd34a28b2a5a43284b4491
joshuaism <joshuaism@msn.com>
2021-03-12 20:48:44 -0600

    Demo the new AdService

--- android/src/com/spacecitygames/adservicedemo/AndroidLauncher.java
+++ android/src/com/spacecitygames/adservicedemo/AndroidLauncher.java
@@ -1,15 +1,37 @@
 package com.spacecitygames.adservicedemo;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.spacecitygames.adservices.AdMobService;
+
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		String adMobKey = getAdMobKey();
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new AdServiceDemo(), config);
+		initialize(new AdServiceDemo(new AdMobService(this, adMobKey)), config);
+	}
+
+	private String getAdMobKey() {
+		if (BuildConfig.DEBUG) {
+			Log.i("ADS", "Running in debug mode. Defaulting to test value.");
+			return AdMobService.TEST_KEY;
+		}
+		try {
+			String packageName = getPackageName();
+			ApplicationInfo ai = getPackageManager().getApplicationInfo(packageName, PackageManager.GET_META_DATA);
+			Bundle bundle = ai.metaData;
+			return bundle.getString("com.google.android.gms.ads.APPLICATION_ID");
+		} catch (Exception e) {
+			Log.e("ADS", "Failed to load com.google.android.gms.ads.APPLICATION_ID from AndroidManifest.xml", e);
+		}
+		return "";
 	}
 }
--- core/src/com/spacecitygames/adservicedemo/AdServiceDemo.java
+++ core/src/com/spacecitygames/adservicedemo/AdServiceDemo.java
@@ -5,15 +5,32 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.input.GestureDetector.GestureListener;
+import com.badlogic.gdx.math.Vector2;
+import com.spacecitygames.adservices.AdService;
 
-public class AdServiceDemo extends ApplicationAdapter {
+public class AdServiceDemo extends ApplicationAdapter implements GestureListener {
 	SpriteBatch batch;
 	Texture img;
+	AdService adService;
+
+	public AdServiceDemo() {
+		this.adService = new AdService() {
+
+		};
+	}
+
+	public AdServiceDemo(AdService adService) {
+		this.adService = adService;
+	}
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		GestureDetector gd = new GestureDetector(this);
+		Gdx.input.setInputProcessor(gd);
 	}
 
 	@Override
@@ -29,5 +46,51 @@ public class AdServiceDemo extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+	}
+
+	@Override
+	public boolean touchDown(float x, float y, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean tap(float x, float y, int count, int button) {
+		adService.showInterstitialAd();
+		return true;
+	}
+
+	@Override
+	public boolean longPress(float x, float y) {
+		return false;
+	}
+
+	@Override
+	public boolean fling(float velocityX, float velocityY, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean pan(float x, float y, float deltaX, float deltaY) {
+		return false;
+	}
+
+	@Override
+	public boolean panStop(float x, float y, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean zoom(float initialDistance, float distance) {
+		return false;
+	}
+
+	@Override
+	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+		return false;
+	}
+
+	@Override
+	public void pinchStop() {
+
 	}
 }
