2a3768a568f0b097db2cf820c1f7c891d81d6611
joshuaism <joshuaism@msn.com>
2021-03-12 20:28:12 -0600

    Initial commit

---
+++ desktop/src/com/spacecitygames/adservicedemo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.spacecitygames.adservicedemo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.spacecitygames.adservicedemo.AdServiceDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AdServiceDemo(), config);
+	}
+}
